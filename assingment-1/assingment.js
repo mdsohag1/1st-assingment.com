1ef6b76dce9745217d5640c0b6011a1a06cdbcc3
mdsohag1 <abbasa90608@gmail.com>
2022-01-18 04:17:27 -0800

    first commit

---
+++ assingment-1/assingment.js
@@ -0,0 +1,58 @@
+// ১. Variable কি ?
+
+    // ans: variable holo amon akti jinis jekhane aponi kunu akti data ke store kore rakhthe parent.poyujone setike bar bar bebohar korthe parent,man bodlateo parent.
+
+// ২. Variable কিভাবে লিখতে হয় ?
+
+    //ans: pothome var diye tar por veriable ar nam diyhe hoy.tarpor equal sine diye veriable ar man ta bosathe hoy.
+    // ex, var num = '20'
+
+// ৩. string type variable কি ও কি ভাবে লিখতে হয় ?
+
+    //ans: string type variable holo ,je variable ar man cotetion ar vitore thakbe setike bujay.ex---
+    // var num = "210"
+
+// ৪. number type variable কি ও কি ভাবে লিখতে হয় ?
+    //ans: number type variable holo ,je variable ar man cotetion ar vitore thake na setike bujay.ex---
+    // var num = 20;
+
+// ৫. Boolan type variable  কি ও কি ভাবে লিখতে হয় ?
+    //ans: je variable ar man nirdisto thake (true/false) setike bujay.ex---
+    // var num = true;
+
+// ৬. toUpperCase() & toLowerCase() এর ব্যাবহার কি ভাবে করতে হয় ? 
+    //ans: var name = "My name is MD Ali Abbas Sohag"
+    // name.toUpperCase();
+
+    // var name = "My name is MD Ali Abbas Sohag"
+    // name.toLowerCase();
+
+// ৭. JavaScript এর মোট কয়টি অপারেটর আছে ও কি কি ?
+    //ans: javascript a mot 7 ti oparetor ase----ex,
+    // + , - , * , / , % , ++ , -- ,
+
+
+
+// ৮. Math.abs() এর ব্যাবহার লিখুন । 
+    //ans: var num = -20;
+    // var sum = Math.abs(num);
+
+// ৯. Math.celi()  এর ব্যাবহার লিখুন । 
+    //ans: var num = 20.235;
+    // var sum = Math.ceil(num);
+
+// ১০. Math.Floor() এর ব্যাবহার লিখুন । 
+    //ans: var num = 20.235;
+    // var sum = Math.floor(num);
+
+// ১১. Math.round() এর ব্যাবহার লিখুন ।
+    //ans: var num = 20.235;
+    // var sum = Math.round(num);
+
+// ১২. Math.random() এর ব্যাবহার লিখুন ।
+    //ans: var num = Math.random(num) * 100;
+ 
+// asignment.js - GitHub Link
+
+
+
---
+++ helllo.js
@@ -0,0 +1,61 @@
+// var school = "adarsho";
+// console.log(school);
+
+// var school = "adarsho";
+// console.log(typeof school);
+
+
+// var ali = 15223;
+// console.log(typeof ali);
+
+// var school = false;
+// console.log(typeof school);
+
+
+
+// var abbas = "SEPARATED THEY LIVE IN BOOKMARKSGROVE RIGHT AT THE COAST OF THE SEMANTICS, A LARGE LANGUAGE OCEAN.FAR FAR AWAY, BEHIND THE WORD MOUNTAINS, FAR FROM THE COUNTRIES VOKALIA AND CONSONANTIA, THERE LIVE THE BLIND TEXTS. SEPARATED THEY LIVE IN BOOKMARKSGROVE RIGHT AT THE COAST OF THE SEMANTICS, A LARGE LANGUAGE OCEAN.SEPARATED THEY LIVE IN BOOKMARKSGROVE RIGHT AT THE COAST OF THE SEMANTICS, A LARGE LANGUAGE OCEAN.FAR FAR AWAY, BEHIND THE WORD MOUNTAINS, FAR FROM THE COUNTRIES VOKALIA AND CONSONANTIA, THERE LIVE THE BLIND TEXTS. SEPARATED THEY LIVE IN BOOKMARKSGROVE RIGHT AT THE COAST OF THE SEMANTICS, A LARGE LANGUAGE OCEAN.."
+// console.log(abbas.toLowerCase());
+
+
+// var ali = "sohag ali abbas emon aksor"
+// console.log(ali.indexOf("ak"))
+
+
+// var ali = 20;
+// var sohag = "40";
+
+// sohag = parseFloat(sohag);
+// var total = ali+sohag;
+// console.log(total);
+
+
+
+// var ali = -20;
+// var ali = Math.abs(ali)
+// console.log(ali);
+
+
+// var sohag = 20.95
+// var sohag = Math.round(sohag)
+// console.log(sohag)
+
+// var ali = 20.23;
+// var ali = Math.ceil(ali)
+// console.log(ali)
+
+
+// var ali = 20.23;
+// var ali = Math.floor(ali)
+// console.log(ali)
+
+
+// var ali = Math.random(ali) * 1000;
+// var ali = Math.round(ali)
+// console.log(ali)
+
+
+
+
+
+
+
